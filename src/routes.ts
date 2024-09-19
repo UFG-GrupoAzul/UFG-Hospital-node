b89a6c294b419c1890a5f8cc5694d26f06658d95
Danillo Tomaz Parreira <parreira16@gmail.com>
2024-09-18 22:54:21 -0300

    Atualização rotas

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import employeeRoute from "./employees/employee.route";
 import specialtyRoute from "./specialties/specialty.route";
 import {authController} from "./auth/auth.controller";
 import hospitalRoute from "./hospital/hospital.route";
+import regulatoryDoctorRoute from "./regulatoryDoctors/regulatoryDoctor.route";
 
 const router = Router();
 
@@ -12,7 +13,8 @@ router.use("/auth", authRoute);
 //router.use("/users", userRoute);
 router.use("/users", authController.authMiddleware, userRoute);
 router.use("/employees", authController.authMiddleware, employeeRoute);
+router.use("/regulatoryDoctors", authController.authMiddleware, regulatoryDoctorRoute);
 router.use("/specialties", authController.authMiddleware, specialtyRoute);
-router.use("/hospital",  authController.authMiddleware, hospitalRoute);
+router.use("/hospital", authController.authMiddleware, hospitalRoute);
 
 export {router};
