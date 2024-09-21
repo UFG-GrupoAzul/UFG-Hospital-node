03d1815b1bbc7bfe82427069a1ed64cc344bbe2a
lsampaiio <121614839+lsampaiio@users.noreply.github.com>
2024-09-20 22:42:12 -0300

    feat(service): Adicionado métodos para gerenciar pacientes

--- src/Pacient/patient.service.ts
+++ src/Pacient/patient.service.ts
@@ -1,15 +1,101 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const patientRoutes = require('patient.routes');
+import { PrismaClient, Patient } from '@prisma/client';
+import { prisma } from "../index";
 
-const app = express();
+class PatientService {
+    prisma: PrismaClient;
+
+    constructor() {
+        this.prisma = prisma;
+    }
+
+    async create(data: Omit<Patient, 'id'>): Promise<Patient> {
+        const { birthDate, name } = data;
 
 
+        const patientExists = await this.findByName(name);
+        if (patientExists) {
+            throw new Error("Patient already exists in the database");
+        }
 
-app.use(bodyParser.json());
+        try {
+            return await this.prisma.patient.create({
+                data: {
+                    birthDate: new Date(birthDate),
+                    name,
+                },
+            });
+        } catch (error) {
+            console.log(`Error creating patient: ${error}`);
+            throw error;
+        }
+    }
 
-app.use('/patients', patientRoutes);
+    async update(id: string, data: Omit<Patient, 'id'>): Promise<Patient> {
+        const patientExists = await this.findById(id);
+        if (!patientExists) {
+            throw new Error("The Patient does not exist in the database.");
+        }
 
-app.listen(port, () => {
-    console.log(`Server is running`);
-});
+        try {
+            return await this.prisma.patient.update({
+                where: { id },
+                data: {
+                    birthDate: new Date(data.birthDate),
+                    name: data.name,
+                },
+            });
+        } catch (error) {
+            console.log(`Error when updating Patient: ${error}`);
+            throw error;
+        }
+    }
+
+    async findById(id: string): Promise<Patient | null> {
+        try {
+            return await this.prisma.patient.findUnique({
+                where: { id },
+            });
+        } catch (error) {
+            console.log(`Error when searching for patient: ${error}`);
+            throw error;
+        }
+    }
+
+    async findByName(name: string): Promise<Patient | null> {
+        try {
+            return await this.prisma.patient.findFirst({
+                where: { name },
+            });
+        } catch (error) {
+            console.log(`Error when searching for patient by name: ${error}`);
+            throw error;
+        }
+    }
+
+    async findAll(): Promise<Patient[]> {
+        try {
+            return await this.prisma.patient.findMany();
+        } catch (error) {
+            console.log(`Error when searching for patients: ${error}`);
+            throw error;
+        }
+    }
+
+    async delete(id: string): Promise<void> {
+        const patientExists = await this.findById(id);
+        if (!patientExists) {
+            throw new Error("The Patient does not exist in the database.");
+        }
+
+        try {
+            await this.prisma.patient.delete({
+                where: { id },
+            });
+        } catch (error) {
+            console.log(`Error deleting patient: ${error}`);
+            throw error;
+        }
+    }
+}
+
+export { PatientService };
