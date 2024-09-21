234efe3fe58cd1d0bf40f2e52b6e490f2b7b4f1e
alexbeserra <alexbeserrab@gmail.com>
2024-09-21 00:05:04 -0300

    Criando medicalRecord

---
+++ src/medicalRecord/medicalRecord.service.ts
@@ -0,0 +1,90 @@
+import {prisma} from "../index";
+
+class MedicalRecordService {
+
+    async create(data: any) {
+        try {
+            const { prescribedDrugs, patientId } = data;
+
+            return await prisma.medicalRecord.create({
+                data: {
+                    prescribedDrugs: {
+                        create: prescribedDrugs
+                    },
+                    patientId: patientId
+                },
+                include: {
+                    prescribedDrugs: true
+                }
+            });
+        } catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async update(id: string, number: string, observation: string) {
+        try {
+            return await prisma.transferDocument.update({
+                where: {id},
+                data:  {
+                    number,
+                    observation
+                }
+            });
+        }catch (error) {
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async getAll(){
+        try{
+            return await prisma.transferDocument.findMany({
+                select: {
+                    id: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    number: true,
+                    observation: true,
+                    requestId: true
+                }
+            })
+        }catch (error){
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async getById(id: string){
+        try{
+            return await prisma.transferDocument.findUnique({where:{id:id},
+                select: {
+                    id: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    number: true,
+                    observation: true,
+                    requestId: true
+                }
+            });
+        }catch (error){
+            console.error(error);
+            throw error;
+        }
+    }
+
+    async delete(id: string){
+        try{
+            await prisma.transferDocument.delete({
+                where:{id}
+            })
+
+        }catch (error){
+            console.log(`Error deleting document transfer: ${error}`);
+            throw error;
+        }
+    }
+}
+
+export {MedicalRecordService};
