91fe953e3757fd37a20f5f058159da386322f29e
mathiastsq <mathias.matos@townsq.com.br>
2024-09-24 21:43:30 -0300

    Transfer concluded CRUD
    Aguardando merge/criação dos outros objetos que Transfer precisa pra ser completo.

--- src/transfer/transfer.controller.ts
+++ src/transfer/transfer.controller.ts
@@ -1,6 +1,7 @@
-import {Request, Response} from 'express';
+import {NextFunction, Request, Response} from "express";
 import {TransferService} from "./transfer.service";
-import {UserService} from "../users/user.service";
+import {Util} from "../utils/util";
+
 
 class TransferController {
     private transferService: TransferService;
@@ -36,6 +37,77 @@ class TransferController {
             console.error(`Error creating Transfer: ${error}`);
             return res.status(500).json({ error: 'Failed to create transfer.' });
         }
+    }
+
+    update = async (req: Request, res: Response) => {
+        const id = req.params.id
+        const {
+            originDoctorId,
+            destinationDoctorId,
+            // patientId,
+            timeOfExit,
+            // requestId,
+            regulatoryDoctorId
+        } = req.body;
+
+        const parsedTimeOfExit = await this.parseTimeOfExit(timeOfExit);
+
+        try{
+            const transferUpdated = await this.transferService.update(id,originDoctorId,destinationDoctorId,parsedTimeOfExit,regulatoryDoctorId)
+            return res.status(201).json(transferUpdated);
+        }catch(error){
+            console.error(`Error updating transfer: ${error}`);
+            return res.status(500).json({ error: 'Failed to update transfer' });
+        }
+
+    }
+
+    findById = async (req: Request, res: Response) => {
+        const id = req.params.id
+        try{
+            const transfer = await this.transferService.findById(id)
+            return res.status(200).json(transfer)
+        }catch(error){
+            console.error(`Error finding transfer: ${error}`);
+            return res.status(404).json({error: 'Failed to find transfer'});
+        }
+    }
+
+    findAll = async (req: Request, res: Response) => {
+        try{
+            const transfers =  await this.transferService.findAll();
+            return res.status(200).json(transfers);
+        }catch(error){
+            console.error(`Error finding transfers: ${error}`);
+            return res.status(500).json({ error: 'Failed to find all transfers' });
+        }
+    }
+
+    delete = async (req: Request, res: Response) => {
+        const id = req.params.id;
+        try{
+            await this.transferService.delete(id)
+            return res.status(204).json({msg: "Transfer deleted"});
+        }catch(error){
+            console.error(`Error deleting transfer: ${error}`);
+            return res.status(500).json({ error: 'Failed to delete transfer'});
+        }
+    }
+
+    verifyIfExists = async (req: Request, res: Response, next: NextFunction) => {
+        const id = req.params.id;
+        Util.validId(id);
+        try {
+            const transfer = await this.transferService.findById(id)
+            if (!transfer) {
+                return res.status(404).json({error: "No transfer found with this ID"});
+            }
+            return next()
+        }catch(error){
+            console.error(`Error finding transfer: ${error}`);
+        }
+
+
     }
 
     private async parseTimeOfExit(timeOfExit: string) {
--- src/transfer/transfer.service.ts
+++ src/transfer/transfer.service.ts
@@ -51,6 +51,65 @@ class TransferService {
             throw error;
         }
     }
+
+    async update(id: string,
+        originDoctorId: string, destinationDoctorId: string,
+        //patientId: string,
+        timeOfExit: Date = new Date(),
+        //requestId: string,
+        regulatoryDoctorId: string){
+
+        const eta = this.generateEta(timeOfExit);
+
+        try{
+            return await prisma.transfer.update({
+                where:{id},
+                data: {
+                    //id: requestId, // Usando o requestId como o ID da Transfer
+                    originDoctorId,
+                    destinationDoctorId,
+                    //patientId,
+                    timeOfExit,
+                    regulatoryDoctorId,
+                    eta,
+                    //requestId: requestId, // Agora você usa o requestId diretamente
+                }
+            })
+        }catch (error){
+            console.error(`Error updating Transfer: ${error}`);
+            throw error;
+        }
+
+    }
+
+    async delete(id: string){
+        try{
+            await prisma.transfer.delete({where:{id}})
+        }catch (error){
+            console.error(`Error deleting transfer: ${error}`);
+            throw error;
+        }
+    }
+
+    async findById(id: string){
+        try{
+            return await prisma.transfer.findUnique({where:{id}})
+        }catch (error){
+            console.error(`Error fetching transfer by ID: ${error}`);
+            throw error;
+        }
+    }
+
+    async findAll(){
+        try{
+            return await prisma.transfer.findMany({})
+        }catch (error){
+            console.error(`Error fetching all transfers: ${error}`);
+            throw error;
+        }
+    }
+
 }
+
 
 export { TransferService };
