29cda78901602cdbc97b8a6f97c82f91a2774f9c
Danillo Tomaz Parreira <parreira16@gmail.com>
2024-11-09 16:23:21 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,7 @@ const app = express();
 app.use(express.json());
 
 // Declara o CORS antes das rotas
-app.use(cors()); // Permite todas as origens (ou especifique 'http://127.0.0.1:3000' para mais segurança)
-
+app.use(cors({ origin: "http://127.0.0.1:3000" }));
 app.use(router);
 
 app.listen(3001, () => console.log("Tecnologia suficientemente avançada é dificilmente diferenciável de magia.\nEsse Trabalho vale uma pizza ☺ . "));
